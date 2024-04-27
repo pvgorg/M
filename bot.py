87a986273d99c6539934b70dc932824972ce8121
Alireza <104611074+pvgorg@users.noreply.github.com>
2024-04-27 14:27:53 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
-import telebot
+from pyrogram import Client, filters
 
-TOKEN = '6153975130:AAFkPPUqX6mCcCxfvYkIurJHl6o2yIUu9yc'
-bot = telebot.TeleBot(TOKEN)
+app = Client("my_bot", api_id=23136380, api_hash="6ae6541159e229499615953de667675c", bot_token="6153975130:AAFkPPUqX6mCcCxfvYkIurJHl6o2yIUu9yc")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Welcome! Use /mute to mute a user and /unmute to unmute a user.")
-
-@bot.message_handler(commands=['mute'])
-def mute_user(message):
+@app.on_message(filters.command("mute"))
+def mute_user(client, message):
     chat_id = message.chat.id
     user_id = message.reply_to_message.from_user.id
-    bot.restrict_chat_member(chat_id, user_id, until_date=0)
+    client.restrict_chat_member(chat_id, user_id, until_date=None, can_send_messages=False)
 
-@bot.message_handler(commands=['unmute'])
-def unmute_user(message):
+@app.on_message(filters.command("unmute"))
+def unmute_user(client, message):
     chat_id = message.chat.id
     user_id = message.reply_to_message.from_user.id
-    bot.restrict_chat_member(chat_id, user_id, until_date=None)
+    client.restrict_chat_member(chat_id, user_id, until_date=None, can_send_messages=True)
 
-bot.polling()
+app.run()
